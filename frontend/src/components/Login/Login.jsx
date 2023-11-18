da7bc467d14d6e40cb474c23126bbb8db80bc891
nikunj05122 <6018.nike@gmail.com>
2023-11-18 16:12:01 +0530

    login screen is complated.

--- frontend/src/components/Login/Login.jsx
+++ frontend/src/components/Login/Login.jsx
@@ -3,36 +3,25 @@ import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
 import "./Login.css";
-import React, { useState } from 'react';
 import line from "./../../assets/icons/line.svg";
 import map from "./../../assets/img/map.png";
-import { useNavigate } from 'react-router-dom';
 import login_scree_logo from "./../../assets/img/login_scree_logo.png";
 import { BASE_SERVER_URL } from "./../../config/constant";
 
 const Login = () => {
-
     const [isLoading, setIsLoading] = useState(false);
     const navigate = useNavigate();
 
-    const handleLogin = async (event) => {
-        event.preventDefault();
-        setIsLoading(true);
-
-          // Simulate a login process for DEMO
-        setTimeout(() => {
-            setIsLoading(false);
-            navigate('/next-page'); // Replace '/next-page' with your actual route
-        }, 3000); // Simulating a network request delay
-    };
-
-    const navigate = useNavigate();
+    // const navigate = useNavigate();
 
     const [number, setNumber] = useState();
     const [MPIN, setMPIN] = useState();
     const [loginData, setLoginData] = useState();
     const handleSubmit = (event) => {
         event.preventDefault();
+        console.log("click");
+        setIsLoading(true);
+
         axios({
             method: "post",
             url: `${BASE_SERVER_URL}/users/login`,
@@ -46,6 +35,8 @@ const Login = () => {
         })
             .then((response) => {
                 console.log("response", response);
+                setIsLoading(false);
+
                 if (response.status === 200) {
                     setLoginData(response.data);
                     navigate("/");
@@ -66,44 +57,62 @@ const Login = () => {
                 <div className="loginPartChild" />
                 <img className="loginPartItem" alt="" src={line} />
                 <div className="leftSide">
-                    <div className="imgcontainer">
+                    <div className="rAK">R A K S H A K</div>
                     <img
-                        className="ab131D9f643638a05C8bd6056eIcon"
+                        className="leftSide-img"
                         alt=""
                         src={login_scree_logo}
                     />
-                    </div>
-                    <b className="rAK">R A K S H A K</b>
                     <div className="v11">v 1.1 - Team Dash</div>
                 </div>
                 <div className="rightSide">
                     <b className="login1">Login</b>
 
+                    <form onSubmit={handleSubmit}>
+                        <input
+                            type="tel"
+                            className="phoneNumberChild"
+                            id="phoneNumber"
+                            name="phoneNumber"
+                            pattern="[0-9]{10}"
+                            maxLength="10"
+                            minLength="10"
+                            value={number}
+                            onChange={(e) => setNumber(e.target.value)}
+                            placeholder="Phone Number"
+                            required
+                        />
 
-                 <form onSubmit={handleLogin}>
-                    <div className="phoneNumber">
-                        {/* <div className="phoneNumberChild" /> */}
-                        <input type="tel" className="phoneNumberChild" id="phoneNumber" name="phoneNumber" pattern="[0-9]{10}" placeholder="Phone Number" required/>
-                        {/* <div className="enterThePin">Phone Number</div> */}
-                       
-                    </div>
+                        <input
+                            type="password"
+                            className="phoneNumberChild"
+                            id="mpin"
+                            name="mpin"
+                            pattern="[0-9]{4}"
+                            maxLength="4"
+                            minLength="4"
+                            value={MPIN}
+                            onChange={(e) => setMPIN(e.target.value)}
+                            placeholder="Enter the PIN"
+                            required
+                        />
 
-                    <div className="mpin">
-                        {/* <div className="phoneNumberChild" /> */}
-                        <input type="text" className="phoneNumberChild" id="mpin" name="mpin" pattern="[0-9]{4}" placeholder="Enter the PIN" required />
-                        {/* <div className="enterThePin">Enter the PIN</div> */}
-                    </div>
-                    <div className="loginButton">
-                        {/* <div className="loginButtonChild" /> */}
-                        <button type="submit" className="login2 loginButtonChild loginButton" disabled={isLoading}>
-                        {isLoading ? <div className="spinner"></div> : 'Login'}
+                        <button
+                            type="submit"
+                            className="login2"
+                            disabled={isLoading}
+                        >
+                            {isLoading ? (
+                                <div className="spinner"></div>
+                            ) : (
+                                "Login"
+                            )}
                         </button>
-                    </div>
-                 </form>
+                    </form>
                 </div>
             </div>
         </div>
     );
-    };
+};
 
 export default Login;
