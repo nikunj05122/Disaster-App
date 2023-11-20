876202ac5e43a274c5fd3b38558654bc1d8f7b93
nikunj05122 <6018.nike@gmail.com>
2023-11-20 13:00:49 +0530

    alert model type field datafield is chaged.

--- Backend/models/AlertArea.js
+++ Backend/models/AlertArea.js
@@ -1,23 +1,34 @@
 const mongoose = require('mongoose');
 
-const AlertAreaSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: [true, 'Provide the name of the alert area.']
-    },
-    location: {
-        type: String,
-        features: [{
-            id: String,
+const AlertAreaSchema = new mongoose.Schema(
+    {
+        name: {
             type: String,
-            properties: Object,
-            geometry: {
-                type: String,
-                coordinates: Array
-            }
-        }]
+            required: [true, 'Provide the name of the alert area.']
+        },
+        location: {
+            type: {
+                type: String
+            },
+            features: [{
+                id: String,
+                type: {
+                    type: String
+                },
+                properties: Object,
+                geometry: {
+                    type: {
+                        type: String
+                    },
+                    coordinates: Array
+                }
+            }]
+        }
+    },
+    {
+        timestamps: true
     }
-});
+);
 
 const AlertArea = mongoose.model('AlertArea', AlertAreaSchema);
 
