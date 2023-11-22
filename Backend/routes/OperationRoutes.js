465ef49ec1418d6d8cbcd6e08d18c53b9553b5b1
nikunj05122 <6018.nike@gmail.com>
2023-11-22 13:32:01 +0530

    operation routes is complate

--- Backend/routes/OperationRoutes.js
+++ Backend/routes/OperationRoutes.js
@@ -1,24 +1,26 @@
 const express = require('express');
 const multer = require('multer');
-// const admin = require('firebase-admin');
-// const fcm = require('')
 
 const authController = require('./../controllers/authController');
 const operationController = require('./../controllers/operationController');
 const addImage = require('./../controllers/firebaseController');
-// const config = require('./../constant/configFireBase');
-
-
 
 const storage = multer.memoryStorage();
 const upload = multer({ storage }).array("file");
 
 const router = express.Router();
 
+router.use(authController.protect, authController.restrictTo(ADMIN, SUPER_ADMIN, OFFICER));
+
 router
     .route('/')
     .get(operationController.getAll)
     .post(upload, addImage, operationController.createOne)
 
+router
+    .route('/:id')
+    .get(operationController.getOne)
+    .patch(operationController.updateOne)
+    .delete(operationController.deleteOne);
 
 module.exports = router;
