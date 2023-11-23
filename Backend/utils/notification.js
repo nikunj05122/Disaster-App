7c7b7cf11235b189879e24c94b6b537e1f767e66
nikunj05122 <6018.nike@gmail.com>
2023-11-23 15:19:29 +0530

    web notification is complate form backend side

--- Backend/utils/notification.js
+++ Backend/utils/notification.js
@@ -1,31 +1,16 @@
-// const firebase = require('firebase-admin');
-
 const catchAsync = require('./catchAsync');
-const { admin, messaging } = require('./../controllers/firebaseAdminController');
-
-// const { firebaseAdminConfig } = require('./../constant/configFireBase');
-
-// firebase.initializeApp({
-//     credential: firebase.credential.cert(firebaseAdminConfig),
-//     databaseURL: 'https://rakshak-main-hackathon.firebaseio.com',
-// });
-
-// const messaging = firebase.messaging();
+const { messaging } = require('./../controllers/firebaseAdminController');
 
 exports.sendNotificationOnApp = catchAsync(async (fcm_token, doc) => {
     console.log("fcm_tokens : ", fcm_token)
-    //this may vary according to the message type (single recipient, multicast, topic, et cetera)
+
     const message = {
         token: fcm_token,
-        // priority: 'high',
-        // webpush: {
-        // },
         notification: {
             title: 'Emergency Alert',
             body: 'Emergency Alert'
         },
 
-        //you can send only notification or only data(or include both)
         data: {
             operation: JSON.stringify({
                 operationId: doc.id,
@@ -41,35 +26,17 @@ exports.sendNotificationOnApp = catchAsync(async (fcm_token, doc) => {
         .catch((error) => {
             console.error('Error sending notification:', error);
         });
-
-    // return admin.messaging().sendAll(message)
-    //     .then((response) => {
-    //         console.log('Successfully sent message:', response);
-    //         if (response.failureCount > 0) {
-    //             // Iterate through responses to get detailed error information
-    //             response.responses.forEach((resp, index) => {
-    //                 if (!resp.success) {
-    //                     console.error(`Failed to send to device ${index + 1}:`, resp.error);
-    //                 }
-    //             });
-    //         }
-    //     })
-    //     .catch((error) => {
-    //         console.error('Error sending message:', error);
-    //     });
 });
 
 exports.sendNotificationOnWeb = catchAsync(async (web_token, doc) => {
     console.log(web_token)
     //this may vary according to the message type (single recipient, multicast, topic, et cetera)
     const message = {
-        tokens: web_token,
-        priority: 'high',
+        token: web_token,
         webpush: {
             notification: {
                 title: 'Emergency Alert',
-                body: 'Emergency Alert',
-                sound: "default"
+                body: 'Emergency Alert'
             },
         },
 
