7d41d7a5bc6d2674d885bcf5471ac35a37e58699
Prarthana2604 <prarthanapatel2604@gmail.com>
2023-11-25 11:38:27 +0530

    Officerrequestcard

---
+++ frontend/src/data/dummydata.js
@@ -0,0 +1,9 @@
+const dummyOfficerData = {
+    name: "Name",
+    address: "1234 Firefighter Rd.",
+    Department: "Punagam firestation",
+    designation: "Senior-officer",
+    phoneNumbers: ['123-456-7890', '987-654-3210'], 
+    imageUrl: "./../../assets/img/map.png"
+  };
+  export default dummyOfficerData;
